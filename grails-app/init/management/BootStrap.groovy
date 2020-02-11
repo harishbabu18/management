64ca2232bde9d7d7a54ebd71ffa926c995f35171
harishbabusdc <harish.babu@sdc.company>
2020-02-11 14:05:51 +0530

    Added to Main DB

--- grails-app/init/management/BootStrap.groovy
+++ grails-app/init/management/BootStrap.groovy
@@ -12,12 +12,17 @@ class BootStrap {
 
     def init = { servletContext ->
         User admin = new User(username: "babuamuda@gmail.com",password:"B@vana20").save()
+        User qualificaadmin = new User(username: "audit@qualificagroup.it",password:"test@123").save()
+
         User customer = new User(username: "liricsdash2014@gmail.com",password:"lirics@123").save()
         User account = new User(username: "akshitharajappa10@gmail.com",password:"akshitha@123").save()
         Role adminrole = new Role(authority: "ROLE_ADMIN").save()
         Role customerrole = new Role(authority: "ROLE_CUSTOMER").save()
         Role accountrole = new Role(authority: "ROLE_ACCOUNT").save()
+
         UserRole.create(admin,adminrole)
+        UserRole.create(qualificaadmin,adminrole)
+
         UserRole.create(customer,customerrole)
         UserRole.create(account,accountrole)
 
